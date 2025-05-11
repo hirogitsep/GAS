7800479773ca02cfe9b087745d6c5594ac1f6d66
hiro <hiroka.kashiwakura@gmail.com>
2025-05-11 12:23:48 +0900

    ヘッダーコメントの追加

--- GAS転記スクリプト.js
+++ GAS転記スクリプト.js
@@ -1,7 +1,17 @@
-// 転記スクリプト（SpreadSheet -> SpreadSheet）
+/*
+ * ==========================================================================
+ * File: GAS転記スクリプト.js
+ * Created: 2025-05-10
+ * Description: 
+ * 転記用汎用スクリプト（SpreadSheet -> SpreadSheet）
+ *  ・転記タイミングは手動実行またはGASのトリガー設定から行う
+ *  ・TODOコメントを参照の上、PJに合わせて変更を行い、使用する
+ *  ・初回のみsetProperties関数のコメントを外し、実行する
+ *    （scriptPropertiesを現在に設定することで過去のデータが再転記されないようにする）
+ * ==========================================================================
+ */
 
-// 初回実行時のみコメントアウトを外し、
-// scriptPropertiesを現在に設定することで過去のデータが再転記されないようにする
+// 初回実行時のみ下記3行のコメントアウトを外す(行選択 → Ctrl + /)
 // function setProperties() {
 //   PropertiesService.getScriptProperties().setScriptProperties(new Date());
 //   return;
@@ -17,18 +27,18 @@ function transferFormData() {
   // 現在時刻を取得
   const now = new Date();
 
-  // 転記元情報
+  // 転記元情報（TODO: PJに合わせて適宜変更）
   const sourceSpreadsheet = SpreadsheetApp.openById('転記元URL');
   const sourceSheet = sourceSpreadsheet.getSheetByName('転記元シート名');
   const sourceData = sourceSheet.getDataRange().getValues();
   const sourceHeaders = sourceData[0];
 
-  // 転記先情報
+  // 転記先情報（TODO: PJに合わせて適宜変更）
   const targetSpreadsheet = SpreadsheetApp.openById('転記元URL');
   const targetSheet = targetSpreadsheet.getSheetByName('転記先シート名');
   const targetHeaders = targetSheet.getDataRange().getValues()[0];
 
-  // マッピング（転記元項目名：転記先項目名）
+  // 転記元項目名：転記先項目名マッピング（TODO: PJに合わせて適宜変更）
   const headerMapping = {
     'Timestamp': '入力日時',
     '名前': '会社名',
@@ -38,17 +48,17 @@ function transferFormData() {
   // 新規追加行
   const newRows = [];
 
-  // 転記データを配列に代入
+  // 転記データを配列に格納
   for (let i = 1; i < sourceData.length; i++) {
     const row = sourceData[i];
 
     // 入力日時を取得
     const timestamp = lastRun ? new Date(row[sourceHeaders.indexOf('Timestamp')]) : null;
 
-    // 新しいデータかどうかを判定
+    // 新規追加データかどうかを判定
     const isNew = lastRun ? (timestamp > lastRun && timestamp <= now) : false;
 
-    // 新しいデータであれば代入
+    // 新規追加データであれば処理実行
     if (isNew) {
       const newRow = [];
       for (let j = 0; j < targetHeaders.length; j++) {
@@ -66,21 +76,21 @@ function transferFormData() {
     }
   }
   
-  // 新規データがあれば転記
+  // 転記処理
   if (newRows.length > 0) {
     targetSheet.getRange(targetSheet.getLastRow() + 1, 1, newRows.length, targetHeaders.length).setValues(newRows);
     
     const sheet = SpreadsheetApp.getActiveSpreadsheet();
     const sheetUrl = sheet.getUrl();
 
-    // メール通知：転記した新しいデータの件数を通知
+    // メール通知：転記データの件数を通知（TODO: メールアドレスを追加する）
     MailApp.sendEmail({
       to: '任意のメールアドレス',
       subject: `GAS 転記処理 実行結果 (${newRows.length} 件)`,
       body: `${newRows.length} 件の新規登録データを転記しました。\n\n${sheetUrl}\n\n実行時刻: ${now.toLocaleString()}\n前回実行: ${lastRun ? lastRun.toLocaleString() : '初回実行'}`
     });
   } else {
-    // 新規データがない場合の通知
+    // 新規データがない場合の通知（TODO: メールアドレスを追加する）
     MailApp.sendEmail({
       to: '任意のメールアドレス',
       subject: `GAS 転記処理 実行結果 (0 件)`,
